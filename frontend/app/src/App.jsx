ab32a0ad537481d856171bf4c77439f430ca8611
Vidya Bhandari <vidyabhandari2804@gmail.com>
2025-08-04 13:51:43 +0530

    app.js updated

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
